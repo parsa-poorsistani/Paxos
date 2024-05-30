7e9c8d40fc4f683375043f19bd9ce0b659aa0cf5
Parsa Poorsistani <poorsistani13@gmail.com>
2024-05-30 22:51:24 +0330

    multi-paxos skeleton

---
+++ multi-paxos/multi-paxos.go
@@ -0,0 +1,267 @@
+package multipaxos
+
+import (
+    "math/rand"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
+
+// This struct represents the state of a single paxos.
+// Each Paxos node maintains a map of PaxosInstance
+// It's the log of entry commands
+type PaxosInstance struct {
+    maxSeen int64
+    acceptedNum int64
+    acceptedValue interface{}
+}
+
+type PaxosNode struct {
+    mu sync.Mutex
+    id int
+    instances map[int]*PaxosInstance
+    peers []string
+    server  *rpc.Server
+    listener  net.Listener
+    currentTerm int64
+    isLeader bool
+    leaderID  int
+    statePath string
+    heartbeatChan chan bool
+    electionTimeout time.Duration
+    hearbeatInterval  time.Duration
+}
+
+type Proposal struct {
+    Number int64
+    Value interface{}
+}
+
+type PrepaerArgs struct {
+    Instance int
+    ProposalNumber int64
+}
+
+type PrepareReply struct {
+    Promise       bool
+    AcceptedNum   int64
+    AcceptedValue interface{}
+}
+
+// RPC arguments and replies for Accept phase.
+type AcceptArgs struct {
+    Instance  int
+    Proposal Proposal
+}
+
+type AcceptReply struct {
+    Accepted bool
+}
+
+// RPC arguments and replies for Learn phase.
+type LearnArgs struct {
+    Instance int
+}
+
+type LearnReply struct {
+    Value interface{}
+}
+
+type AddCommandArgs struct {
+    Value interface{}
+}
+
+type AddCommandReply struct {
+    CommandNum int
+}
+
+// RPC arguments and replies for listing commands.
+type ListCommandsArgs struct{}
+
+type ListCommandsReply struct {
+    Commands map[int]interface{}
+}
+
+type RequestVoteArgs struct {
+    Term        int64
+    CandidateID int
+}
+
+type RequestVoteReply struct {
+    VoteGranted bool
+}
+
+// RPC arguments and replies for Heartbeat phase.
+type HeartbeatArgs struct {
+    Term     int64
+    LeaderID int
+}
+
+type HeartbeatReply struct{}
+
+
+func NewPaxosNode(id int, peers []string, addr string, statePath string) *PaxosNode {
+    node := &PaxosNode{
+        id: id,
+        peers: peers,
+        instances: make(map[int]*PaxosInstance),
+        statePath: statePath,
+        isLeader: false,
+        currentTerm: 0,
+        leaderID: -1,
+        heartbeatChan: make(chan bool),
+        hearbeatInterval: time.Duration(500 * time.Millisecond),
+        electionTimeout: time.Duration(1000 + rand.Intn(1501)) * time.Millisecond,
+    }
+    
+    node.loadState()
+    node.server = rpc.NewServer()
+    node.server.Register(node)
+
+    listener, err := net.Listen("tcp", addr)
+
+    if err != nil {
+        panic(err)
+    }
+    node.listener = listener
+    go node.acceptConn()
+    go node.startElectionTimeout()
+
+    return node
+}
+
+
+func (pn *PaxosNode) acceptConn() {
+    for {
+        conn, err := pn.listener.Accept()
+        if err != nil {
+            //panic(err) TODO: error handling
+            continue
+        }
+        go pn.server.ServeConn(conn)
+    }
+}
+
+// loadState loads the acceptor state from a file.
+func(pn *PaxosNode) loadState() {
+    pn.mu.Lock()
+    defer pn.mu.Unlock()
+
+    if _, err := os.Stat(pn.statePath); os.IsNotExist(err) {
+        return
+    }
+
+    data, err := os.ReadFile(pn.statePath)
+    if err != nil {
+        panic(err)
+    }
+    
+    state := make(map[int]*PaxosInstance)
+    if err := json.Unmarshal(data, &state); err != nil {
+        fmt.Print("panic at unmarshal\n")
+        panic(err)
+    }
+
+    pn.instances = state
+}
+
+// saveState saves the acceptor state to a file.
+func (pn *PaxosNode) saveState() {
+
+    pn.mu.Lock()
+    defer pn.mu.Unlock()
+
+    state := pn.instances 
+
+    data, err := json.Marshal(state)
+    if err != nil {
+        panic(err)
+    }
+
+    if err := os.WriteFile(pn.statePath, data, 0644); err != nil {
+        panic(err)
+    }
+}
+
+
+func(pn *PaxosNode) startElectionTimeout() {
+    for {
+        select {
+            case <- pn.heartbeatChan:
+                fmt.Printf("Node %d: received Heartbeat, resseting election timeout\n",pn.id)
+            // not sure about this electionTimeoout
+            case <- time.After(pn.electionTimeout):
+                //election timeout, starting a new election
+                fmt.Printf("Node %d: Election timeout, starting new election\n", pn.id)
+                pn.startElection()
+            // quiteChan?
+        }
+    }
+}
+
+func(pn *PaxosNode) startElection() {
+    pn.mu.Lock()
+    pn.currentTerm++
+    term := pn.currentTerm
+    pn.mu.Unlock()
+    
+    votes := 1
+
+    var voteMu sync.Mutex
+    var voteWg sync.WaitGroup
+    for _, peer := range pn.peers {
+        voteWg.Add(1)
+        go func(peer string) {
+            defer voteWg.Done()
+            args := RequestVoteArgs{
+                Term: term,
+                CandidateID: pn.id,
+            }
+            var reply RequestVoteReply
+            if Call(peer, "PaxosNode.VoteRequest", args, &reply) && reply.VoteGranted {
+                voteMu.Lock()
+                votes++
+                voteMu.Unlock()
+            }
+
+        }(peer)
+    } 
+    voteWg.Wait()
+
+    if votes > len(pn.peers)/2 {
+        pn.mu.Lock()
+        pn.isLeader = true
+        pn.leaderID = pn.id
+        pn.mu.Unlock()
+        go pn.sendHearbeats()
+    }
+}
+
+
+
+func Call(addr, rpcMethod string, args interface{}, reply interface{}) bool {
+    client, err := rpc.Dial("tcp",addr)
+    if err != nil {
+        fmt.Errorf("Error in Call method %e", err)
+        return false
+    }
+
+    defer client.Close()
+    call := client.Go(rpcMethod, args, reply, nil)
+    
+    select {
+        case <- call.Done:
+            return call.Error == nil
+        case <- time.After(1 * time.Second):
+            fmt.Printf("timeout for RPC call with %s, for addr: %s",rpcMethod, addr)
+            return false
+    }
+
+}
+
+
+
